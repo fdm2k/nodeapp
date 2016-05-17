e0a679c7f480c4e19d53872ffbaad7530a00b529
Adam <fdm_2k@hotmail.com>
2016-05-17 15:04:37 +0930

    Complete overhaul to content and style

--- server.js
+++ server.js
@@ -1,50 +1,43 @@
 var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// Setup the app using Express
 var app = express();
-var os = require('os');
 
 // Setup the web server host and port
 const host = process.env.INTERNAL_HOST || 'http://localhost';
 const port = process.env.INTERNAL_PORT || 3000;
-const server_hostname = os.hostname() || 'HOSTNAME-NOT-SET';
-const server_ostype = os.type();
-const server_osplatform = os.platform();
-var server_ostotalmem = os.totalmem();
-var server_osfreemem = os.freemem();
-
-var ejs = require('ejs');
-var path = require('path');
 
 // view engine setup
 app.engine('html', ejs.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 					// log every request to the console
+app.use(bodyParser()); 						// pull information from html in POST
+app.use(methodOverride()); 					// simulate DELETE and PUT
+
 // serve an empty page that just loads the browserify bundle
 app.get('/', function(req, res) {
-  var drinks = [
-    { name: 'Bloody Mary', drunkness: 3 },
-    { name: 'Martini', drunkness: 5 },
-    { name: 'Scotch', drunkness: 10 }
-  ];
-  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+  var pagename = "Home";
 
   res.render('pages/index', {
-    drinks: drinks,
-    tagline: tagline
+    pagename: pagename
   });
 });
 
 app.get('/about', function(req, res) {
+  var pagename = "About";
   res.render('pages/about', {
-    server_hostname: server_hostname,
-    server_ostype: server_ostype,
-    server_osplatform: server_osplatform,
-    server_ostotalmem: server_ostotalmem,
-    server_osfreemem: server_osfreemem
+    pagename: pagename
   });
 });
 
 app.listen(port);
 
-console.log('Your hostname is: '+os.hostname());
 console.log('Listening on port '+port+'. Open up '+host+':'+port+'/ in your browser.');
